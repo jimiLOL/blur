a35e6ae34e6b2cf6dba68e162702f8e767f3f388
John Hopkins <cov12ssd@gmail.com>
2023-04-01 19:46:55 +0300

    Множественные BID

--- src/checkBid.js
+++ src/checkBid.js
@@ -240,7 +240,7 @@ const check = async (accountAvailable) => {
                     }
                     ele[price].time = new Date().getTime();
 
-                    if (!bid && ele[price].bidderCount >= 5 || bid_obj?.count?.count == 0) {
+                    if (!bid && ele[price].bidderCount >= 5 || bid_obj?.count?.count < 20) {
                         const s = ele[price].total_eth > BlurPoolClass.walletSetBalance[account.walletAddress].balance * 3 && Number(BlurPoolClass.walletSetBalance[account.walletAddress].balance) >= Number(price) && checkMinPrice(price, key);
                         if (await s) {
                             // console.log('already bid ' + price);
